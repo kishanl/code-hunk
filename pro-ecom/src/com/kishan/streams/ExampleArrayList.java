2c48e4c762f9c20bc7fbf9bf49007d7381788a45
unknown <kishan-lalwani>
2026-02-16 10:10:22 +0530

    Example Arrray List class

---
+++ pro-ecom/src/com/kishan/streams/ExampleArrayList.java
@@ -0,0 +1,15 @@
+package com.kishan.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExampleArrayList {
+
+	public static void main(String[] args) {
+		List<String> asianCountries = Arrays.asList("INDIA","BHUTAN","NEPAL");		
+		System.out.println(asianCountries);
+		
+
+	}
+
+}
